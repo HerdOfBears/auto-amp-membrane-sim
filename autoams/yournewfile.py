10d5a798bec62c30d84ccfa8cac976f5002f7dc1
jyler <jyler.menard@mail.concordia.ca>
2023-10-11 10:37:56 -0400

    new file function import test

---
+++ autoams/yournewfile.py
@@ -0,0 +1,8 @@
+
+__all__ = ['newfile_function']
+
+def newfile_function(a):
+    print(f"Hello, {a}! I am in a new file")
+
+def newfile_function2(a):
+    print(f"Hello, {a}! I am also in a new file. But I can't be seen")
