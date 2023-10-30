89bdf54975c0a44f55b64f6bc69359b5b4b5f8c0
natalyawatson <natalya.watson@mail.concordia.ca>
2023-10-30 17:44:04 -0400

    filled in test function for PeptideGrid object: step 3 of workflow

---
+++ autoams/tests/test_step3.py
@@ -0,0 +1,16 @@
+from __future__ import absolute_import, division, print_function
+import os.path as op
+import numpy as np
+import pandas as pd
+import numpy.testing as npt
+import autoams as amps
+
+data_path = op.join(amps.__path__[0], 'data')
+
+
+def test_PeptideGrid(pdb = 'testpdb.pdb'):
+    '''Test peptide grid object'''
+    model = PeptideGrid(pdb)    #create the object that will generate the grid of peptides
+    model.GenerateGrid()        #generate the grid with default settings
+    model.SaveGrid()            #save the 3d coordinates to a pdb file renamed to all_molecules_combined.pdb
+
